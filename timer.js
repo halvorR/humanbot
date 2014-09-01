3dad17186ac8fc6e8984966ea6475ccca4fcca24
tanktopbroski <blawtekjar@gmail.com>
2014-09-01 17:26:57 +0200

    script for refreshing
    satt til å redirecte til en venteside.html hver halvtime xx:29 og xx:59

---
+++ timer.js
@@ -0,0 +1,138 @@
+// Definerer starttidspunktet
+var now = new Date();
+
+var time1 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 29, 0, 0) - now;
+if (time1 < 0) {
+     time1 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time1);
+
+var time2 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 59, 0, 0) - now;
+if (time2 < 0) {
+     time2 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time2);
+
+var time3 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 11, 29, 0, 0) - now;
+if (time3 < 0) {
+     time3 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time3);
+
+
+var time4 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 11, 59, 0, 0) - now;
+if (time4 < 0) {
+     time4 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time4);
+
+var time5 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 29, 0, 0) - now;
+if (time5 < 0) {
+     time5 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time5);
+
+var time6 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12, 59, 0, 0) - now;
+if (time6 < 0) {
+     time6 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time6);
+
+var time7 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 13, 29, 0, 0) - now;
+if (time7 < 0) {
+     time7 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time7);
+
+var time8 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 13, 59, 0, 0) - now;
+if (time8 < 0) {
+     time8 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time8);
+
+
+//Endreblokk på for testing//
+///////////////////////////////////////////////////////////////////////////////////////////
+
+var time9 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 29, 0, 0) - now;
+if (time9 < 0) {
+     time9 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time9);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//Slutt på endreblokk//
+
+var time10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 59, 0, 0) - now;
+if (time10 < 0) {
+     time10 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time10);
+
+var time11 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 29, 0, 0) - now;
+if (time11 < 0) {
+     time11 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time11);
+
+var time12 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 59, 0, 0) - now;
+if (time12 < 0) {
+     time12 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time12);
+
+var time13 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 16, 29, 0, 0) - now;
+if (time13 < 0) {
+     time13 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time13);
+
+var time14 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 16, 59, 0, 0) - now;
+if (time14 < 0) {
+     time14 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time14);
+
+var time15 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 17, 29, 0, 0) - now;
+if (time15 < 0) {
+     time15 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time15);
+
+var time16 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 17, 59, 0, 0) - now;
+if (time16 < 0) {
+     time16 += 86400000;
+}
+setTimeout(function() {
+	window.location.assign("ventesiden.html");
+}, time16);
